a694514940206b160adf9d12695610fe3e2b2904
Silvan Joos <silvan.joos@stud.fhgr.ch>
2024-10-17 17:38:33 +0200

    add merger-script for one dir

---
+++ src/CSV_Merger.py
@@ -0,0 +1,29 @@
+import os
+import pandas as pd
+import glob
+
+
+def FileMerger_Days():
+
+    folder = 1
+
+    for i in range(0,31):
+    
+
+        all_files = glob.glob(rf'C:\Users\silva\Documents\CDS\DS\Fitness-Data-Projekt\data\2024-08\CSV\CSV_DATA\{folder}\WELLNESS/*.csv',recursive=True)
+        all_df = []
+        for f in all_files:
+            df = pd.read_csv(f, sep=',')
+            all_df.append(df)
+
+        df=all_df[0]
+        for df_temp in all_df[1:]:
+            df = pd.concat([df, df_temp], ignore_index=True, sort=False)
+        #removing duplicate monitoring.timestamp[s] due to tracking error
+        df_clean = df.drop_duplicates(subset=['monitoring.timestamp[s]'])
+        df_clean.to_csv(rf'C:\Users\silva\Documents\CDS\DS\Fitness-Data-Projekt\data\merged\{folder}.csv',sep=',')
+        folder += 1
+
+
+
+FileMerger_Days()
